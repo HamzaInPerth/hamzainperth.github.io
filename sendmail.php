aa0e561ea73e1852d30f9329cd8da870d96d48cc
hamzainperth <chikhi.h@gmail.com>
2020-09-20 09:52:37 +0200

    header security fixed

--- sendmail.php
+++ sendmail.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://hamzainperth.github.io");
 header('Content-Type: application/json');
 
 $firstname = htmlspecialchars($_POST['firstname']);
