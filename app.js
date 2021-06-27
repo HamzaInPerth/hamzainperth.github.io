e5816c21a63637e5858e9cfaf78a52ba74e74844
Hamza Chikhi <chikhi.h@gmail.com>
2021-06-27 11:11:39 +0200

    fix: form, links

--- app.js
+++ app.js
@@ -1,84 +1,66 @@
+("use strict");
 
-(function () {
+// VARIABLE
+var title = document.querySelector("#title");
 
-    // VARIABLE
-    var title = document.querySelector("#title");
+// HEIGHT OF DOM
+var screenHeight =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
 
-    ("use strict");
-    // HEIGHT OF DOM
-    var screenHeight =
-      window.innerHeight ||
-      document.documentElement.clientHeight ||
-      document.body.clientHeight;
+// DISPLAY PHONE NUMBER
+var phone = document.querySelector("#phone");
+phone.addEventListener("click", function () {
+  part1 = "(+33) 07 49 ";
+  part2 = "03 00 03";
+  phone.innerHTML = part1 + part2;
+});
 
-    // DISPLAY PHONE NUMBER
-    var phone = document.querySelector("#phone");
-    phone.addEventListener("click", function () {
-      part1 = "(+33) 07 49 ";
-      part2 = "03 00 03";
-      phone.innerHTML = part1 + part2;
-    });
+//HEIGHT OF DOM (ONRESIZE)
+window.onresize = function () {
+  screenHeight =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight;
+};
 
-    //HEIGHT OF DOM (ONRESIZE)
-    window.onresize = function () {
-      screenHeight =
-        window.innerHeight ||
-        document.documentElement.clientHeight ||
-        document.body.clientHeight;
-    };
-
-
-
-    /////
-
-    function effectOnWorkItem() {
-        if (window.innerWidth < 900) {
-     //     addEventListener('scroll', function() {
-                var workItem = document.querySelectorAll(".work-item");
-                Array.prototype.forEach.call(workItem, e => {
-
-                    sections[e.id] = e.offsetTop - screenHeight / 2;
-                })
-
-    //   })
-        }
-    }
-
-
-
-    /////
-    section = document.querySelectorAll(".sections");
-    title = document.querySelector("#titletext");
-    let sections = {};
-    Array.prototype.forEach.call(section, (e) => {
+function effectOnWorkItem() {
+  if (window.innerWidth < 900) {
+    var workItem = document.querySelectorAll(".work-item");
+    Array.prototype.forEach.call(workItem, (e) => {
       sections[e.id] = e.offsetTop - screenHeight / 2;
     });
+  }
+}
 
-    effectOnWorkItem()
+section = document.querySelectorAll(".sections");
+title = document.querySelector("#titletext");
+let sections = {};
+Array.prototype.forEach.call(section, (e) => {
+  sections[e.id] = e.offsetTop - screenHeight / 2;
+});
 
+effectOnWorkItem();
 
-    addEventListener("resize", effectOnWorkItem)
+addEventListener("resize", effectOnWorkItem);
 
-    // var scrollPosition =
-    //   document.documentElement.scrollTop || document.body.scrollTop;
+// ONSCROLL CHANGE COLOR OF ACTIVE LINK
+window.onscroll = () => {
+  var scrollPosition =
+    document.documentElement.scrollTop || document.body.scrollTop;
 
-    // ONSCROLL CHANGE COLOR OF ACTIVE LINK
-    window.onscroll = () => {
-      var scrollPosition =
-        document.documentElement.scrollTop || document.body.scrollTop;
+  if (scrollPosition <= 100) {
+    titleOpacity = 1 - scrollPosition / 100;
+    title.style.opacity = titleOpacity;
+  }
 
-      if (scrollPosition <= 100) {
-        titleOpacity = 1 - scrollPosition / 100;
-        title.style.opacity = titleOpacity;
-      }
-
-      for (i in sections) {
-        if (sections[i] <= scrollPosition) {
-          document
-            .querySelector(".active-link")
-            .classList.remove("active-link");
-          document.querySelector("a[href*=" + i + "]").classList.add("class", "active-link");
-        }
-      }
-    };
-  })();
+  for (i in sections) {
+    if (sections[i] <= scrollPosition) {
+      document.querySelector(".active-link").classList.remove("active-link");
+      document
+        .querySelector("a[href*=" + i + "]")
+        .classList.add("class", "active-link");
+    }
+  }
+};
