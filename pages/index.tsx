33df54a3908e1490818960bf7fca2b1f203ecbc7
Hamza Chikhi <chikhi.h@gmail.com>
2022-09-18 13:43:04 -0400

    style: on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,11 +26,11 @@ const Index = () => {
             alt: "Play chess with me",
             link: 'https://lichess.org/@/hamzainparis',
         },
-        {
-            picture: buyMeCoffee,
-            alt: 'Buy me a coffee',
-            link: 'https://buymeacoffee.com/hamzafromparis',
-        }
+        // {
+        //     picture: buyMeCoffee,
+        //     alt: 'Buy me a coffee',
+        //     link: 'https://buymeacoffee.com/hamzafromparis',
+        // }
     ]
     return (
         <div className="min-h-screen p-10  tracking-widest leading-loose">
@@ -50,16 +50,20 @@ const Index = () => {
                         <li><b>Framework front :</b> Bootstrap, Tailwind, Vuejs, Nuxtjs, jQuery</li>
                         <li><b>Framework back :</b> Laravel, Lumen</li>
                     </ul>
-                    <p><b className="text-cyan-700">Some random things I am use to : </b>Conventional Commits, Jira, Docker, JWT, Redis, VueX, Axios, SEO, React, NextJS, Googling...</p>
+                    <p><b className="text-cyan-700">Some random things I am use to : </b>Conventional Commits, Jira, Docker, JWT, Redis, VueX, Pinia, Axios, SEO, React, NextJS, Googling stuffs...</p>
                     <i className="text-gray-500">This portfolio has been made with NextJs.</i>
-                    <span><Link href="/works"><a className="underline hover:no-underline font-semibold text-2xl tracking-widest">Check out my Works</a></Link></span>
+                    <div className="m-auto">
+                        <Link href="/works">
+                            <a className="font-semibold text-xl tracking-widest px-4 py-2 bg-black border-2 border-black hover:bg-white hover:text-black rounded-lg transition-all ease-in-out duration-300 text-white">Check out my Works</a>
+                        </Link>
+                    </div>
                 </div>
                 <div className="w-3/5 border-2 border-black mt-3 mb-10 rounded-full bg-black h-2"></div>
                 <p className="font-light text-xl">When it comes to interact with humans, I use english, or french.</p>
 
                 <div className="flex flex-wrap w-full overflow-hidden h-auto">
                     {links.map(function (element, index) {
-                        return <div key={index} className="block h-24 m-3 relative bg-pink-400"  >
+                        return <div key={index} className="block h-24 m-3 relative">
                             <a href={element.link} target="_blank"><Image
                                 placeholder='blur'
                                 loader={imageLoader}
@@ -68,9 +72,6 @@ const Index = () => {
                                 width={'100%'}
                                 height={'100%'}
                                 layout="fixed"
-                                sizes="(min-width: 400px) 33vw,
-                                (min-width: 60px) 50vw,
-                                100vw"
                             /></a>
                         </div>
                     })}
